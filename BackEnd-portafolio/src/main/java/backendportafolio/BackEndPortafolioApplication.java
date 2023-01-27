090a9104f55803dd3a0486514133f2f926338d57
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2023-01-27 09:48:57 -0500

    firts commit

---
+++ BackEnd-portafolio/src/main/java/backendportafolio/BackEndPortafolioApplication.java
@@ -0,0 +1,13 @@
+package backendportafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndPortafolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndPortafolioApplication.class, args);
+    }
+
+}
---
+++ BackEnd-portafolio/src/test/java/backendportafolio/BackEndPortafolioApplicationTests.java
@@ -0,0 +1,13 @@
+package backendportafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndPortafolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
