68c34df608bccc25990da5f6d9f8071442853945
Diego Orlando Gonzalez Cruz <dogc.1994@gmail.com>
2023-12-15 11:50:37 -0500

    update

--- BackEnd-portafolio/src/main/java/backendportafolio/dtos/responses/ResumenResultadosResponse.java
+++ BackEnd-portafolio/src/main/java/backendportafolio/dtos/responses/ResumenResultadosResponse.java
@@ -1,10 +1,12 @@
 package backendportafolio.dtos.responses;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Builder
 @EqualsAndHashCode
+@Data
 public class ResumenResultadosResponse {
 
     private String prueba;
--- BackEnd-portafolio/src/main/java/backendportafolio/service/implementations/DocumentManageService.java
+++ BackEnd-portafolio/src/main/java/backendportafolio/service/implementations/DocumentManageService.java
@@ -1,6 +1,10 @@
 package backendportafolio.service.implementations;
 
+import backendportafolio.dtos.responses.ResumenResultadosResponse;
 import backendportafolio.integrations.S3DocumentManage;
+import backendportafolio.repository.contracts.IPruebasRepository;
+import backendportafolio.repository.contracts.IResultadosRepository;
+import backendportafolio.repository.entities.PruebasEntity;
 import backendportafolio.service.contracts.IDocumentManageService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +25,8 @@ import software.amazon.awssdk.services.s3.model.S3Object;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -29,6 +35,8 @@ public class DocumentManageService implements IDocumentManageService {
 
     S3DocumentManage s3DocumentManage;
     private final S3Client s3Client;
+    IPruebasRepository iPruebasRepository;
+    IResultadosRepository iResultadosRepository;
 
     @Override
     public void uploadProfileEstudentImage(MultipartFile multipartFile, int userId) throws IOException {
@@ -47,12 +55,24 @@ public class DocumentManageService implements IDocumentManageService {
         PDPage page = document.getPage(0);
 
         try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
-            // Agrega contenido a la página según tus necesidades
+
             contentStream.setFont(PDType1Font.HELVETICA_BOLD, 12);
-            contentStream.beginText();
-            contentStream.newLineAtOffset(100, 700);
-            contentStream.showText("Contenido dinámico dsafdsafsdfasdfasdfsadfasdfasdfasdfasdfasdfasdfasdf");
-            contentStream.endText();
+            // Agrega contenido a la página según tus necesidades
+            String[] lineas = retroAlimentacion(userId).split("\n");
+
+            // Establecer la posición inicial
+            float yPosition = 650;
+
+            for (String linea : lineas) {
+                // Agregar texto a la página
+                contentStream.beginText();
+                contentStream.newLineAtOffset(50, yPosition);
+                contentStream.showText(linea);
+                contentStream.endText();
+
+                // Mover a la siguiente línea
+                yPosition -= 15; // Ajusta según sea necesario
+            }
         }
 
         document.save(outputStream);
@@ -64,6 +84,45 @@ public class DocumentManageService implements IDocumentManageService {
                 .build(), RequestBody.fromBytes(outputStream.toByteArray()));
 
         return s3DocumentManage.getFile("tesis-seria-s3","profile-image/", "Hoja-membrete-carta-color.pdf" );
+    }
+
+    private String retroAlimentacion(int estudianteId){
+        var resume = getResumeResultados( estudianteId);
+        final String[] resumenPDF = {"Buenos dias acontinuacion te dejo un resumen de todas las pruebas y retroalimentacion que has tenido de las pruebas que has echo con la app: \n"};
+        resume.forEach(resumenResultadosResponse -> {
+            String prueba = "Prueba : " + resumenResultadosResponse.getPrueba() +"\n"
+                    +"_________________________________________________________________________________________";
+            String codigo = "Solucion que propusiste: " + resumenResultadosResponse.getCodigo() + "\n"
+                    +"_________________________________________________________________________________________";
+
+            String resultado = "Solucion que propusiste: " + resumenResultadosResponse.getResultado() + "\n"
+                    +"_________________________________________________________________________________________";
+
+            resumenPDF[0] += prueba + codigo + resultado;
+        });
+
+        return resumenPDF[0];
+    }
+
+
+    private List<ResumenResultadosResponse> getResumeResultados(int estudianteId) {
+        List<PruebasEntity> pruebasEntityList = iPruebasRepository.getByEstudianteId(estudianteId);
+
+        return pruebasEntityList.stream()
+                .map(prueba -> {
+                    ResumenResultadosResponse.ResumenResultadosResponseBuilder resul = ResumenResultadosResponse.builder()
+                            .prueba(prueba.getContenido());
+
+                    iResultadosRepository.getAllByPruebasId(prueba.getPruebasid())
+                            .forEach(resultadosEntity -> {
+                                resul.codigo(resultadosEntity.getCodigoId())
+                                        .resultado(resultadosEntity.getResultado());
+                            });
+
+                    return resul.build();
+                })
+                .collect(Collectors.toList());
+
     }
 
 
